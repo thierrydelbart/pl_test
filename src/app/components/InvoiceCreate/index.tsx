8e44ae53f593133d9f11d98e1f436c348e62bacd
Thierry Delbart <thierry.delbart@swile.co>
2024-11-07 13:18:49 +0100

    feat(create-invoice): adding validation function

--- src/app/components/InvoiceCreate/index.tsx
+++ src/app/components/InvoiceCreate/index.tsx
@@ -1,49 +1,101 @@
 import { useState } from 'react'
 import { useApi } from 'api'
 import { Customer } from 'types'
-import CustomerAutocomplete from '../CustomerAutocomplete'
 import { Components } from 'api/gen/client'
 import DatePicker from "react-datepicker";
+import CustomerAutocomplete from '../CustomerAutocomplete'
 
 import "react-datepicker/dist/react-datepicker.css";
 
+interface InvoiceCreateProps {
+  customer?: Customer | null,
+  date?: Date | null,
+  deadline?: Date | null,
+}
+
 const InvoiceCreate = () => {
   const api = useApi()
-  const [customer, setCustomer] = useState<Customer | null>(null)
-  const [deadline, setDeadline] = useState<Date | null>(new Date());
+  const [ formData, setFormData ] = useState<InvoiceCreateProps>({
+    date: new Date(),
+    deadline: new Date(),
+  })
 
-  const handleCreate = async () => {
-    if (!customer) return;
+  const validateFormData = (data: InvoiceCreateProps) => {
+    if (!data.customer) {
+      window.alert('Please select a customer');
+      return;
+    }
+    if (!data.date) {
+      window.alert('Please select an invoicing date');
+      return false;
+    }
+    if (!data.deadline) {
+      window.alert('Please select a due date');
+      return false;
+    }
+    if (data.deadline < data.date) {
+      window.alert('Due date should be after invoicing date');
+      return false;
+    }
+    return true
+  }
 
-    let invoice:Components.Schemas.InvoiceCreatePayload = { 
-      customer_id: customer.id,
-      deadline: deadline?.toISOString(),
-      date: new Date().toISOString(),
-    }; 
+  const handleCreate = async (event: React.SyntheticEvent<HTMLFormElement>) =>{
+    event.preventDefault();
 
-    console.log('Creating invoice...');
-    api.postInvoices(null, { invoice: invoice }).then(({ data }) => {
-      alert(`Invoice ${data.id} created successfully!`);
+    if (!validateFormData(formData)) return;
+   
+    const customer_id = formData?.customer?.id;
+    if (!customer_id) return;
+
+    const invoiceCreatePayload: Components.Schemas.InvoiceCreatePayload = {
+      customer_id: customer_id,
+      date: formData.date?.toISOString(),
+      deadline: formData.deadline?.toISOString(),
+    };
+
+    api.postInvoices(null, { invoice: invoiceCreatePayload }).then(({ data }) => {
+      window.alert(`Invoice ${data.id} created successfully!`);
       window.location.href = '/'
     }).catch((error) => {
       window.alert("Error creating invoice\n" + error?.response?.data?.message)
-    })
+    }) 
   };
-  
+
   return (
     <div>
       <h1>New invoice</h1>
       <a className="btn btn-outline-dark mb-3" href="/">Back</a>
-      <div className="mb-3">
-        <label className="form-label">Customer</label>
-        <CustomerAutocomplete value={customer} onChange={setCustomer} />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Due date</label>
-        <br />
-        <DatePicker selected={deadline} onChange={(date) => setDeadline(date)} />
-      </div>
-      <button onClick={handleCreate} className="btn btn-outline-primary">Create</button>
+      <form onSubmit={handleCreate}>
+        <div className="mb-3">
+          <label className="form-label">Customer</label>
+          <CustomerAutocomplete 
+            value={formData?.customer || null} 
+            onChange={(customer) => setFormData({ ... formData, customer: customer})} 
+          />
+        </div>
+        { formData?.customer && (
+          <div>
+            <div className="mb-3">
+              <label className="form-label">Invoice date</label>
+              <br />
+              <DatePicker 
+                selected={formData?.date}
+                onChange={(date) => setFormData({ ... formData, date: date})}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Due date</label>
+              <br />
+              <DatePicker 
+                selected={formData?.deadline}
+                onChange={(date) => setFormData({ ... formData, deadline: date})}
+              />
+            </div>
+          </div>
+        )}
+        <input type="submit" className="btn btn-outline-primary" value="Create" />
+      </form>
     </div>
   )
 }
