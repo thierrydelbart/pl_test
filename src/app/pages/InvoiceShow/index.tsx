991a1a5962616b3142b5f4bae604140a94088df5
Thierry Delbart <thierry.delbart@swile.co>
2024-11-25 22:55:27 +0100

    chore: lint

--- src/app/pages/InvoiceShow/index.tsx
+++ src/app/pages/InvoiceShow/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 
 import { useApi } from 'api'
-import { Customer, Invoice } from 'types'
+import { Invoice } from 'types'
 import { useNavigate } from 'react-router-dom'
 import UpdateInvoiceForm from './updateInvoiceForm'
 import { Components } from 'api/gen/client'
